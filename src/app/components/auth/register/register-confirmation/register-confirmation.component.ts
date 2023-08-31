36bc1ba19441347e6e09d2fc2d38e253d4db81b4
Danieleg <daniel.krusinski@nexteer.com>
2023-08-31 10:32:52 +0200

    rebuild with key component

--- src/app/components/auth/register/register-confirmation/register-confirmation.component.ts
+++ src/app/components/auth/register/register-confirmation/register-confirmation.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -10,54 +11,30 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./register-confirmation.component.css'],
 })
 export class RegisterConfirmationComponent {
-  key!: string;
   keyForm!: FormGroup;
-  private destroy$: Subject<void> = new Subject<void>();
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
-    this.formValidator();
-    this.subscribeFormChanges();
-  }
-
-  formValidator(): void {
-    this.keyForm = this.formBuilder.group({ key: ['', Validators.required] });
-  }
-
-  subscribeFormChanges(): void {
-    this.keyForm.valueChanges
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((formValues) => {
-        this.key = formValues.key;
-      });
-  }
-
-  onSubmit() {
-    if (this.keyForm.valid) {
-      this.authService
-        .RegisterConfirmation(this.key)
-        .pipe(
-          tap((response) => {
-            if (response === true) {
-              this.router.navigate(['/login'])
-              this.authService.canEnterKey = false;
-              console.log(response);
-            } else {
-              this.router.navigate(['/register/confirmation']);
-              console.log(response);
+  onSubmit(key: string) {
+    this.authService
+      .RegisterConfirmation(key)
+      .pipe(
+        tap((response) => {
+          if (response === true) {
+            if (localStorage.getItem('vz2cd')) {
+              localStorage.removeItem('vz2cd');
             }
-          }),
-          catchError((error) => {
-            console.log('looooooooooooooo ', error);
-            throw this.router.navigate(['/register/confirmation']);
-          })
-        )
-        .subscribe();
-    }
+            this.router.navigate(['/login']);
+            console.log(response);
+          } else {
+            console.log(response, 'wrong code');
+          }
+        }),
+        catchError((error) => {
+          console.log('errrrrrrrrror ', error);
+          throw error;
+        })
+      )
+      .subscribe();
   }
 }
