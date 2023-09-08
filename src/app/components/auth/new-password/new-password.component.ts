31f587e0a4504e7150a5795036d79e9b11c8d536
Danieleg <daniel.krusinski@nexteer.com>
2023-09-08 09:42:51 +0200

    add input comp for new/change psw

---
+++ src/app/components/auth/new-password/new-password.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Subject, catchError, takeUntil, tap } from 'rxjs';
+import { NewPassword } from 'src/app/models/NewPassword';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-new-password',
+  templateUrl: './new-password.component.html',
+  styleUrls: ['./new-password.component.css'],
+})
+export class NewPasswordComponent {}
