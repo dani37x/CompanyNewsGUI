abcc0b5fb76d91081b830689b84f94e6710d4f6f
Danieleg <daniel.krusinski@nexteer.com>
2023-10-06 08:03:34 +0200

    add navbar fadeInOut animation

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,9 +1,34 @@
-import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      state(
+        'in',
+        style({
+          opacity: 1,
+        })
+      ),
+      state(
+        'out',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('open => out', [animate('0.7s ease-out')]),
+      transition('out => open', [animate('0.7s ease-in')]),
+    ]),
+  ],
 })
 export class NavbarComponent {
   showHamburger = false;
